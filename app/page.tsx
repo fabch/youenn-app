74a6e38e61fe6ad3d682590d9dd4716736803310
Fabien Chartrain <fabien.chartrain@gmail.com>
2024-09-08 18:03:56 +0200

    Update page.tsx to add materials

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
+import { useState, useEffect } from 'react'
+import styles from '../styles/Home.module.css'
+import supabase from '../utils/supabase'
 import Image from "next/image";
 
 export default function Home() {
+
+  const [todos, setTodos] = useState([])
+
+  const fetchTodos = async () => {
+    const { data } = await supabase.from('material').select('*')
+    setTodos(data)
+  }
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -12,6 +27,9 @@ export default function Home() {
           height={38}
           priority
         />
+        {todos.map((todo) => (
+          <p key={todo.name}>{todo.name} {todo.category}</p>
+        ))}
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
